6dbfb399782dbc5e0d68b82729001c9dc3df6768
Manish Kumar <manishkumarg7394@gmail.com>
2026-06-22 23:05:52 -0700

    Update mix_fun_game.py

--- mix_fun_game.py
+++ mix_fun_game.py
@@ -9,34 +9,34 @@ Ye tumhe real programming jaisa feel dega.
 	•	“Too high” ya “Too low” batayega
 	•	Sahi guess → “You Win”'''
 
-# secret_number = 7
-# while True:
-#     guess = int(input("guess the number"))
-#     if guess == secret_number:
-#         print("you winn 🎉 ")
-#         break
-#     elif guess > secret_number:
-#         print("too High ❌")
-#     else :
-#         print("too low ❌")
+ secret_number = 7
+ while True:
+     guess = int(input("guess the number"))
+     if guess == secret_number:
+         print("you winn 🎉 ")
+         break
+     elif guess > secret_number:
+         print("too High ❌")
+     else :
+         print("too low ❌")
 
 '''Isme ek aur feature add karo:
 👉 User ko sirf 3 chances mile'''
-# secret_namber = 7
-# attempts = 3
-# guess = int(input( "guess the number"))
-# while attempts > 0:
-#     if guess == secret_namber:
-#         print("you winn")
-#     elif guess> secret_namber:
-#         print("Too High ")
-#     else :
-#         print("Too Low")
-#     attempts -=1
-#     print(f"chances left: {attempts}")
+ secret_namber = 7
+ attempts = 3
+ guess = int(input( "guess the number"))
+ while attempts > 0:
+     if guess == secret_namber:
+         print("you winn")
+     elif guess> secret_namber:
+         print("Too High ")
+     else :
+         print("Too Low")
+     attempts -=1
+     print(f"chances left: {attempts}")
 
-# if attempts == 0:
-#     print("game over")
+ if attempts == 0:
+     print("game over")
 '''
     🔹 4️⃣ Functions Practice
 	16.	Ek function banao jo do numbers ka sum return kare.
@@ -46,15 +46,15 @@ Ye tumhe real programming jaisa feel dega.
 	20.	Ek function banao jo kisi number ka square aur cube dono return kare.'''
 
 # 16.	Ek function banao jo do numbers ka sum return kare.
-# def add_numbers(a, b):
-#     return a + b
+ def add_numbers(a, b):
+     return a + b
 
-# result = add_numbers(10, 5)
-# print("Sum is:", result)
+ result = add_numbers(10, 5)
+ print("Sum is:", result)
 
-# def add_num(a,b,c):
-#     return a+b+c
-# result = add_num(12,14,64)
-# print("sum is :",result)
+def add_num(a,b,c):
+     return a+b+c
+ result = add_num(12,14,64)
+ print("sum is :",result)
 
 # 17.	Ek function banao jo number even/odd check kare.
